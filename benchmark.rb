da5e9e88fcb2fd1d6ff26aa9c9415d41827db384
Valar <a.valarpirai@gmail.com>
2019-12-09 18:31:51 +0530

    Update intentdation

--- benchmark.rb
+++ benchmark.rb
@@ -2,19 +2,19 @@ require 'benchmark/ips'
 require 'ruby_xid'
 
 Benchmark.ips do |x|
-    x.config(:time => 5, :warmup => 2)
+  x.config(:time => 5, :warmup => 2)
 
-    x.report('Xid generation') do 
-        Xid.new
-    end
+  x.report('Xid generation') do
+    Xid.new
+  end
 
-    x.report('Xid generate and convert to bytes') do 
-        Xid.new.bytes
-    end
+  x.report('Xid generate and convert to bytes') do
+    Xid.new.bytes
+  end
 
-    x.report('Xid generate and convert to string') do 
-        Xid.new.string
-    end
+  x.report('Xid generate and convert to string') do
+    Xid.new.string
+  end
 
-    x.compare!
+  x.compare!
 end
